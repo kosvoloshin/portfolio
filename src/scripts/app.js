eff288e011a057df37168c0e3ca997b438d1cb6d
Voloshin Konstantin <testformsite2@gmail.com>
2018-03-20 00:09:08 +0200

    Mon2

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,7 @@
 const slider = require('./common/slider');
 
 slider(); // инициализируем слайдер
+
+const map = require('./common/google-maps');
+
+map(); // инициализируем карты
--- src/scripts/common/google-maps.js
+++ src/scripts/common/google-maps.js
@@ -1,13 +1,15 @@
 // google maps
 
 window.initMap = function() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+  var uluru = { lat: 48.9481767, lng: 38.4878774 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      zoomControl: false,
+      scaleControl: true,
+      mapTypeControl: false,
+      streetViewControl: false,
+      rotateControl: false,
+      fullscreenControl: false,
       center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
+  });
+}
