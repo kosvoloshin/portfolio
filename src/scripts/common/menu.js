08e8a8fbf32345541c727767c1fe4e9c511688b3
Voloshin Konstantin <testformsite2@gmail.com>
2018-03-29 19:58:24 +0300

    Tue

--- src/scripts/common/menu.js
+++ src/scripts/common/menu.js
@@ -6,10 +6,12 @@ const menuInit = (function() {
             $('#openFull').on('click', function(e) {
                 e.preventDefault();
                 $('.full-m').toggleClass('active');
+                $('.hamburger').toggleClass('active');
             });
             $('#closeFull').on('click', function(e) {
                 e.preventDefault();
                 $('.full-m').removeClass('active');
+                $('.hamburger').removeClass('active');
             });
         },
     };
