55ef007a723fb2bc18b9946a6777d322b7fc5e41
Voloshin Konstantin <testformsite2@gmail.com>
2018-04-02 00:13:01 +0300

    Sun

--- src/scripts/common/parallax.js
+++ src/scripts/common/parallax.js
@@ -1,4 +1,32 @@
-var parallaxContainer = document.getElementById('parallax'),
+const $ = require('jquery');
+
+if($('#parallax').length){
+    var parallaxContainer = document.getElementById('parallax'),
+    layers = parallaxContainer.children;
+
+    var moveLayers = function (e) {
+    var initialX = (window.innerWidth / 2) - e.pageX;
+    var initialY = (window.innerHeight / 2) - e.pageY;
+
+    [].slice.call(layers).forEach(function(layer, index) {
+      var 
+        divider = index / 100,
+        positionX = initialX * divider,
+        positionY = initialY * divider,
+        bottomPosition = (window.innerHeight / 2) * divider,
+        transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
+        image = layer.firstElementChild;
+
+      layer.style.transform = transformString;
+      image.style.bottom = '-' + bottomPosition + 'px';
+    });
+
+};
+
+window.addEventListener('mousemove', moveLayers);
+}
+
+/*var parallaxContainer = document.getElementById('parallax'),
   layers = parallaxContainer.children;
 
 var moveLayers = function (e) {
@@ -20,4 +48,4 @@ var moveLayers = function (e) {
 
 };
 
-window.addEventListener('mousemove', moveLayers);
+window.addEventListener('mousemove', moveLayers);*/
